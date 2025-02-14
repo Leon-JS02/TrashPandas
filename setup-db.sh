6b1dfe0afeb606960e056d9d93f0a7f141fba925
Leon <leonjs02@protonmail.com>
2025-02-14 16:27:42 +0000

    Added messages to setup file.

--- setup-db.sh
+++ setup-db.sh
@@ -1,4 +1,10 @@
-psql postgres -f schema.sql
+echo "Loading .env"
+source .env
+echo "Creating schema..."
+psql -h $DB_HOST -U $DB_USER postgres -f schema.sql
+echo "Creating virtual environment."
 python3 -m venv venv
 source venv/bin/activate
+echo "Seeding database."
 python3 insert.py
+echo "Setup complete!"
