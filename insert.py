20b63a1626573da16d999712f1cf74df2717c449
Leon <leonjs02@protonmail.com>
2025-02-14 11:16:13 +0000

    Boilerplate.

--- insert.py
+++ insert.py
@@ -2,3 +2,12 @@
 Script for seeding the database according to a pre-defined distribution.
 Seeds tables: 'bin', 'assign_raccoon_clan', 'item_rummage' 
 """
+
+from dotenv import load_dotenv
+
+from psycopg2 import connect
+from psycopg2.extensions import connection, cursor
+from psycopg2.extras import RealDictCursor
+
+if __name__ == "__main__":
+    load_dotenv()
