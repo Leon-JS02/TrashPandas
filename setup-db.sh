560e144f95d98a066137e77b1dd847d2894cb8e0
Leon <leonjs02@protonmail.com>
2025-02-20 12:12:43 +0000

    Modified setup db file to accept optional argument for number of items.

--- setup-db.sh
+++ setup-db.sh
@@ -1,10 +1,16 @@
 echo "Loading .env"
 source .env
+
 echo "Creating schema..."
 psql -h $DB_HOST -U $DB_USER postgres -f schema.sql
+
 echo "Creating virtual environment."
 python3 -m venv venv
 source venv/bin/activate
-echo "Seeding database."
-python3 insert.py
+
+NUM=${1:-500}
+
+echo "Seeding database with $NUM items."
+python3 insert.py -n $NUM
+
 echo "Setup complete!"
